7c51feec283a332d681f9c7cf2b49fda0835eb3c
ammo <ammo@noobuntu.lan>
2023-03-27 22:57:28 +0630

    setup middlewares

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import compression from 'compression'
 
 const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(logger('dev'))
+app.use(cors())
+app.use(helmet())
+app.use(compression())
+
+// routes
+// app.use('')
 
 app.use('/', (req, res) => res.send('OK'))
 
