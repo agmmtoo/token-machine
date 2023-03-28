3a17576e65e6ebbb38777902b43fae8bee40837b
ammo <ammo@noobuntu.lan>
2023-03-29 00:53:02 +0630

    setup routers

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ import cors from 'cors'
 import helmet from 'helmet'
 import compression from 'compression'
 
+import clientRouter from './routes/client.router.js'
+import tokenRouter from './routes/token.router.js'
+
 const app = express()
 
-
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
 app.use(cors())
 app.use(helmet())
 app.use(compression())
 
 // routes
-// app.use('')
+app.use('/api/v1/client', clientRouter)
+app.use('/api/v1/token', tokenRouter)
 
 app.use('/', (req, res) => res.send('OK'))
 
